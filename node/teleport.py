5116b5def45030bf9fc649603eaceced2cade2cf
Andrew Mckell <amckell33@icloud.com>
2025-12-03 00:05:36 -0800

    teleport (needs to be called with terminal input for now)

--- node/teleport.py
+++ node/teleport.py
@@ -4,6 +4,56 @@ import rospy
 from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import SetModelState
 
+"""
+To find robot position: 
+$ rosservice call /gazebo/get_model_state "{model_name: 'B1', relative_entity_name: 'world'}"
+
+Destinations: 
+1. Starting location:
+rosservice call /gazebo/set_model_state "model_state:
+  model_name: 'B1'
+  reference_frame: 'world'
+  pose:
+    position: {x: 5.499995095757394, y: 2.4995765304828543, z: 0.040000462677662746}
+    orientation: {x: 6.342845588236196e-07, y: -4.662894710339661e-07, z: -0.706898193114858, w: 0.7073153077449471}
+  twist:
+    linear:  {x: 0.0, y: 0.0, z: 0.0}
+    angular: {x: 0.0, y: 0.0, z: 0.0}"
+
+2. First pink line:
+rosservice call /gazebo/set_model_state "model_state:
+  model_name: 'B1'
+  reference_frame: 'world'
+  pose:
+    position: {x: 0.628385129930686, y: 0.0006904961777512801, z: 0.040002067617634356}
+    orientation: {x: -1.1011464673110303e-06, y: -7.734997000777081e-07, z: 0.4590277749340737, w: 0.8884219165673832}
+  twist:
+    linear:  {x: 0.0, y: 0.0, z: 0.0}
+    angular: {x: 0.0, y: 0.0, z: 0.0}"
+
+3. Second pink line: 
+rosservice call /gazebo/set_model_state "model_state:
+  model_name: 'B1'
+  reference_frame: 'world'
+  pose:
+    position: {x: -3.9608819384409353, y: 0.45929834454066515, z: 0.040000687022617255}
+    orientation: {x: -2.6994734507055647e-06, y: 3.77677665257361e-07, z: 0.9999987571263347, w: 0.001576622452033168}
+  twist:
+    linear:  {x: 0.0, y: 0.0, z: 0.0}
+    angular: {x: 0.0, y: 0.0, z: 0.0}"
+
+4. Third pink line: 
+rosservice call /gazebo/set_model_state "model_state:
+  model_name: 'B1'
+  reference_frame: 'world'
+  pose:
+    position: {x: -4.030194411692881, y: -2.3089761660560773, z: 0.04000053984738465}
+    orientation: {x: 3.886040698488489e-10, y: 2.0192185688131462e-06, z: -0.0008701127515915282, w: -0.9999996214497896}
+  twist:
+    linear:  {x: 0.0, y: 0.0, z: 0.0}
+    angular: {x: 0.0, y: 0.0, z: 0.0}"
+"""
+
 class Teleport: 
     def __init__(self, model_name="B1", reference_frame="world"):
 
